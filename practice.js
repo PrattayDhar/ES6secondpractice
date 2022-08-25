dd9a708a005a7a7037795943068a1939f601f5ec
PrattayDhar <prattaygub@gmail.com>
2022-08-25 22:19:40 +0100

    practice

---
+++ practice.js
@@ -0,0 +1,17 @@
+// 31 - 2 Access Value, nested object Optional chaining c
+
+const user= [{id:12,name:'hasan', job:'ghum'}]
+// console.log(user[0].name);
+const data ={
+    count :5000,
+    data:[
+        {id:358, name:'Shithi',job:'centy'},
+        {id:063, name:'MIM',job:'bbs'},
+
+    ]
+}
+// console.log(data.data[0].name);
+
+
+// 3 Array map to do one line loop magic
+
