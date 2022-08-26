ab9054508983b811892cb6332569610b423cef27
PrattayDhar <prattaygub@gmail.com>
2022-08-26 12:03:47 +0100

    practice

--- practice.js
+++ practice.js
@@ -14,4 +14,32 @@ const data ={
 
 
 // 3 Array map to do one line loop magic
+const numbers =[2,8,4,6,3];
+const output =[];
+for(const number of numbers){
+    const double =number*2;
+    output.push(double);
+}
+// console.log(output);
 
+function getdouble(numbers){
+    const output= [];
+    for (const number of numbers){
+        const double=number*2
+        output.push(double);
+    }
+    return output;
+}
+const result =getdouble(numbers);
+// console.log(result);
+
+// Make it easy by map 
+
+const dobbl =num=>num*2;
+const makedbl =numbers.map(dobbl);
+// console.log(makedbl); 
+
+const makedbl2= numbers.map(x=>x*2);
+// console.log(makedbl2);
+const fivetimes =[1,2,3,4,5].map(x=>x*3);
+// console.log(fivetimes);
